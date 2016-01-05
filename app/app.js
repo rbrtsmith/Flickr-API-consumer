301ce10f7d128aaa8f2cfff27943d43571f69b47
Robert Smith <robert@wasabiweb.se>
2016-01-05 10:01:29 +0100

    update app

--- app/app.js
+++ app/app.js
@@ -9,9 +9,12 @@ const App = React.createClass({
 		const disselectAll = (data) => data.map(() => false);
 		let selectedImages;
 		if (localStorage.getItem('selectedImages')) {
+			// state is found in localStorage... then get that state
 			selectedImages = JSON.parse(localStorage.getItem('selectedImages'));
 		} else {
+			// No state foun in localStorage so create a new state with all objects disselected
 			selectedImages = disselectAll(data);
+			// then push the new state to localStorage.
 			localStorage.setItem('selectedImages', JSON.stringify(selectedImages));
 		}
 		return {
@@ -19,7 +22,9 @@ const App = React.createClass({
 		};
 	},
 	selectImage(imgIndex) {
+		// get state from localStorage
 		const selectedImages = JSON.parse(localStorage.getItem('selectedImages'));
+
 		const newState = this.state.selectedImages.map((selectedValue, index) => {
 			if (imgIndex === index) {
 				selectedImages[index] = !selectedImages[index];
@@ -28,7 +33,9 @@ const App = React.createClass({
 				return selectedValue;
 			}
 		});
+		// update state in localStorage.
 		localStorage.setItem('selectedImages', JSON.stringify(selectedImages));
+		// force re-render of the app
 		this.setState({
 			selectedImages: newState
 		});
@@ -56,5 +63,5 @@ const App = React.createClass({
 	}
 });
 
-window.app = data => ReactDOM.render( <App data={data.items} />, document.querySelector('#app'));
+window.APP = data => ReactDOM.render( <App data={data.items} />, document.querySelector('#app'));
 
