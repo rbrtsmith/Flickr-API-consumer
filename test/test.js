5f332780e1c5752ad1d9326777cafc79b88b45ee
Robert Smith <rbrtsmith84@googlemail.com>
2016-01-06 22:28:43 +0100

    added more tests

--- test/test.js
+++ test/test.js
@@ -13,7 +13,7 @@ import Img from './../app/components/img';
 
 // Test functions
 describe('toggle-img-highlight', () => {
-    it('should return an array with the boolean at the index reversed.', () => {
+    it('should return an array with highlighted item set to true', () => {
         const index = 1;
         const initialState = [false, false, false];
         const expected = [false, true, false];
@@ -42,7 +42,7 @@ describe('Images', () => {
             }
         ],
         selectImage(){ return null },
-        highlightedImages: [false, false, false]
+        highlightedImages: [false, false]
     };
     it('should be an unordered list.', () =>{
         const renderer = TestUtils.createRenderer();
@@ -63,10 +63,47 @@ describe('Images', () => {
             selectImage={testData.selectImage}
             highlightedImages={testData.highlightedImages}
         />);
-        const actual = renderer.getRenderOutput().props.className;
-        const expected = 'o-grid';
+        const actual = renderer.getRenderOutput().props.className.includes('o-grid');
+        const expected = true;
         expect(actual).toEqual(expected);
     });
+    
+    it('should have 2 children', () => {
+        const renderer = TestUtils.createRenderer();
+        renderer.render(<Images
+            data={testData.data}
+            selectImage={testData.selectImage}
+            highlightedImages={testData.highlightedImages}
+        />);
+        const actual = renderer.getRenderOutput().props.children.length;
+        const expected = 2;
+        expect(actual).toEqual(expected);
+    });
+
+    it('should have lis as children', () => {
+        const renderer = TestUtils.createRenderer();
+        renderer.render(<Images
+            data={testData.data}
+            selectImage={testData.selectImage}
+            highlightedImages={testData.highlightedImages}
+        />);
+        const actual = renderer.getRenderOutput().props.children[0].type;
+        const expected = 'li';
+        expect(actual).toEqual(expected);
+    });
+
+    it('should have children with the correct classname', () => {
+        const renderer = TestUtils.createRenderer();
+        renderer.render(<Images
+            data={testData.data}
+            selectImage={testData.selectImage}
+            highlightedImages={testData.highlightedImages}
+        />);
+        const actual = renderer.getRenderOutput().props.children[0].props.className.includes('o-grid__item');
+        const expected = true;
+        expect(actual).toEqual(expected);
+    });
+
 });
 
 
@@ -99,9 +136,22 @@ describe('Img', () => {
             isSelected={false} 
             src={'http://farm2.staticflickr.com/1482/23548579844_820dd5f1f4_m.jpg'}
         />);
-        const actual = renderer.getRenderOutput().props.className;
-        const expected = 'o-flickr-img';
-        expect(actual).toIncludeJSX(expected);
+        const actual = renderer.getRenderOutput().props.className.includes('o-flickr-img');
+        const expected = true;
+        expect(actual).toEqual(expected);
+    });
+
+    it('should not give a selected class if not favourited.', () => {
+        const renderer = TestUtils.createRenderer();
+        renderer.render(<Img 
+            selectImage={clickHandler} 
+            index={1} 
+            isSelected={false} 
+            src={'http://farm2.staticflickr.com/1482/23548579844_820dd5f1f4_m.jpg'}
+        />);
+        const actual = renderer.getRenderOutput().props.className.includes('selected');
+        const expected = false;
+        expect(actual).toEqual(expected);
     });
 
     it('should give a selected class if favourited.', () => {
@@ -112,8 +162,8 @@ describe('Img', () => {
             isSelected={true} 
             src={'http://farm2.staticflickr.com/1482/23548579844_820dd5f1f4_m.jpg'}
         />);
-        const actual = renderer.getRenderOutput().props.className;
-        const expected = 'o-flickr-img selected';
-        expect(actual).toIncludeJSX(expected);
+        const actual = renderer.getRenderOutput().props.className.includes('selected');
+        const expected = true;
+        expect(actual).toEqual(expected);
     });
 });
