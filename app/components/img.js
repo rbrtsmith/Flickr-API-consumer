8602a2c7c58b5b0e4da9ab0a2aea336b80c8d8c6
Robert Smith <rbrtsmith84@googlemail.com>
2016-01-06 18:56:03 +0100

    convert Img over to a pure component

--- app/components/img.js
+++ app/components/img.js
@@ -1,17 +1,13 @@
 import React from 'react';
 
-export default React.createClass({
-	/**
-	 * This component has no state, it is passed down from the App component.
-	 */
-	render() {
-		const { selectImage, index, isSelected, src } = this.props;
-		// click handler calls function selectImage that was passed from App as a prop
-		const handleClick = (index) => selectImage(index);
-		// set classname based on boolean value of the prop isSelected.
-		const className = isSelected ? 'o-flickr-img selected' : 'o-flickr-img';
-		return (
-			<img src={src} className={className} onClick={handleClick.bind(null, index)}/>
-		);
-	}
-});
+
+export default ({
+	selectImage,
+	index,
+	isSelected,
+	src
+}) => {
+	const handleClick = (index) => selectImage(index);
+	const className = isSelected ? 'o-flickr-img selected' : 'o-flickr-img';
+	return <img src={src} className={className} onClick={handleClick.bind(null, index)}/>;
+}
--- test/test.js
+++ test/test.js
@@ -19,7 +19,6 @@ describe('Img', () => {
             isSelected={false} 
             src={'http://farm2.staticflickr.com/1482/23548579844_820dd5f1f4_m.jpg'}
         />);
-        
         const actual = renderer.getRenderOutput();
         const expected = <img 
             src={'http://farm2.staticflickr.com/1482/23548579844_820dd5f1f4_m.jpg'} 
@@ -27,6 +26,23 @@ describe('Img', () => {
             onClick={clickHandler}
         />;
         expect(actual).toIncludeJSX(expected);
-        
+    });
+
+
+    it('should give a selected class if favourited', () => {
+        const renderer = TestUtils.createRenderer();
+        renderer.render(<Img 
+            selectImage={clickHandler} 
+            index={1} 
+            isSelected={true} 
+            src={'http://farm2.staticflickr.com/1482/23548579844_820dd5f1f4_m.jpg'}
+        />);
+        const actual = renderer.getRenderOutput();
+        const expected = <img 
+            src={'http://farm2.staticflickr.com/1482/23548579844_820dd5f1f4_m.jpg'} 
+            className={'o-flickr-img selected'}
+            onClick={clickHandler}
+        />;
+        expect(actual).toIncludeJSX(expected);
     });
 });
